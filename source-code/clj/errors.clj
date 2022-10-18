19216a9d58526849b6a3c4d5603971fc2b048a65
Bit Handshake <adam@monotech.hu>
2022-10-18 12:46:56 +0200

    m0.6.7 #1 - initial commit

---
+++ source-code/clj/errors.clj
@@ -0,0 +1,40 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns mongo-db.errors)
+
+
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+; @constant (string)
+(def QUERY-MUST-BE-MAP-ERROR "Query must be map")
+
+; @constant (string)
+(def INPUT-MUST-BE-MAP-ERROR "Input must be map")
+
+; @constant (string)
+(def MISSING-NAMESPACE-ERROR "Document must be a namespaced map with keyword type keys")
+
+; @constant (string)
+(def MISSING-DOCUMENT-ID-ERROR "Missing document ID error")
+
+; @constant (string)
+(def MISSING-DOCUMENT-ORDER-ERROR "Missing document order error")
+
+; @constant (string)
+(def DOCUMENT-DOES-NOT-EXISTS-ERROR "Document does not exists error")
+
+; @constant (string)
+(def DOCUMENT-CORRUPTED-ERROR "Document corrupted error")
+
+; @constant (string)
+(def REORDER-DOCUMENTS-FAILED "Reordering documents failed")
+
+; @constant (string)
+(def REMOVING-DOCUMENT-FAILED "Removing document failed")
+
+; @constant (string)
+(def APPLYING-FUNCTION-FAILED "Applying function failed")
---
+++ source-code/clj/postparing.clj
@@ -0,0 +1,25 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns mongo-db.postparing
+    (:require [mid-fruits.candy :refer [return]]))
+
+
+
+;; -- Applying document -------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(defn apply-input
+  ; WARNING! NON-PUBLIC! DO NOT USE!
+  ;
+  ; @param (string) collection-name
+  ; @param (namespaced map) document
+  ; @param (map) options
+  ;  {:postpare-f (function)(opt)}
+  ;
+  ; @return (namespaced map)
+  [collection-name document {:keys [postpare-f] :as options}]
+  (try (if postpare-f (postpare-f document)
+                      (return     document))
+       (catch Exception e (println (str e "\n" {:collection-name collection-name :document document :options options})))))
